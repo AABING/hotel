db604311aec01fab2b49a7f1d81315760799c33e
AABING <18766499074@163.com>
2020-01-31 18:39:14 +0800

    “修正了xml绑定失败的配置，新增用户表、房间表的增删改查”

---
+++ src/main/java/com/face/hotel/controller/RoomController.java
@@ -0,0 +1,12 @@
+package com.face.hotel.controller;
+
+/**
+ * Description: RoomController
+ *
+ * @author LiBingxiang
+ * @version 1.0
+ * @date 2020/01/31 16:57
+ * @since JDK 1.8
+ */
+public class RoomController {
+}
