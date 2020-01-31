b43488735d8cec675b6a712d17cff74799556a3b
MeiyuJijieYihou <184606905@qq.com>
2020-01-31 22:20:35 +0800

    新增FaceRecords Controller类

---
+++ src/main/java/com/face/hotel/controller/FaceRecordsController.java
@@ -0,0 +1,10 @@
+package com.face.hotel.controller;
+
+/**
+ * @Institution csust
+ * @Author MeiyuJijieYihou
+ * @Description Waiting fo development.
+ * @Date 2020/1/23 下午5:45
+ */
+public class FaceRecordsController {
+}
