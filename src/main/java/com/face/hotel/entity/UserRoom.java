872521f4b00c6ea4438615a2df5c2163bf80b95c
MeiyuJijieYihou <184606905@qq.com>
2020-02-02 22:24:14 +0800

    修改UserRoom实体类，同时增加注释。

--- src/main/java/com/face/hotel/entity/UserRoom.java
+++ src/main/java/com/face/hotel/entity/UserRoom.java
@@ -1,5 +1,7 @@
 package com.face.hotel.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.sql.Timestamp;
@@ -15,18 +17,57 @@ import java.sql.Timestamp;
 @Data
 public class UserRoom {
 
+  /**
+   * 用户ID
+   */
+  @ApiModelProperty("用户ID")
   private Long userId;
 
+
+  /**
+   * 房间ID
+   */
+  @ApiModelProperty("房间ID")
   private Long roomId;
 
+
+  /**
+   * 预定时间
+   */
+  @ApiModelProperty("预定时间")
   private Timestamp scheduledTime;
 
+
+  /**
+   * 入住时间
+   */
+  @ApiModelProperty("入住时间")
   private Timestamp checkInTime;
 
-  private Integer duration;
 
+  /**
+   * 退房时间（用户选择那天的12点）
+   */
+  @ApiModelProperty("退房时间（用户选择那天的12点）")
+  private Integer checkOutTime;
+  // private Integer duration;
+
+  /**
+   * 是否生效（前台人证识别成功后生效，即不再退款。当天18点以后未退订自动生效）
+   */
+  @ApiModelProperty("是否生效（前台人证识别成功后生效，即不再退款。当天18点以后未退订自动生效）")
   private Integer effect;
 
+  /**
+   * 是否结清消费金额（查一下用户信息表debt，如果未结清则不可退房）
+   */
+  @ApiModelProperty("是否结清消费金额（查一下用户信息表debt，如果未结清则不可退房）")
   private Integer clearing;
+
+  /**
+   * 用户是否需要停车位：0（不需要），1（需要）
+   */
+  @ApiModelProperty("用户是否需要停车位：0（不需要），1（需要）")
+  private Integer Vehicle;
 
 }
